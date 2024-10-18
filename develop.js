028ed218bcfd36001b15f9c11171cb54daffcd24
kaledoviedoo <137461484+kaledoviedoo@users.noreply.github.com>
2024-10-18 07:46:24 -0500

    Add files via upload

---
+++ develop.js
@@ -0,0 +1,37 @@
+let counter= 0;
+let content = [
+    'Front-end Developer',
+    'Graphic Designer',
+    'UI/UX Designer',
+    'Motion Graphic Designer',
+    'Web Developer',
+    'Branding Designer',
+    'Freelancer',
+]
+let jobTitle = document.querySelector('.job')
+const slide = () => {
+    if(counter >= content.length){
+        counter = 0;
+    }
+    jobTitle.innerHTML   = ' ' 
+    jobTitle.classList.remove('holder-animation');
+    void jobTitle.offsetWidth;
+    jobTitle.classList.add('holder-animation');
+
+    const addLetterDiv = (letter, delay) => {
+        let letterDiv =document.createElement('div');
+        letterDiv.innerHTML = letter === " " ? "&nbsp":
+        letter;
+        letterDiv.classList.add('start', 'animation');
+        letterDiv.style.animationDelay = delay + 's';
+        jobTitle.appendChild(letterDiv);
+    };
+
+    for(let i = 0; i < content[counter].length; i++){
+        addLetterDiv(content[counter][i], i / 10);
+    }
+    counter++;
+};
+
+slide();
+setInterval(slide, 4000);
